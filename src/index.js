4d6102e048d0ee60ffc12b8d7e2a1ce64444ff53
FIRST_NAME LAST_NAME <abalaki03@gmail.com>
2022-01-12 14:05:43 +0400

    .

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import * as Logger from "./logger";
+import * as Utils from "./utils";
+
+const person = {
+    age: 25,
+    address: {
+        street: "vajas street"
+    },
+};
+
+console.log(person.address?.street);
+
+Logger.log("my message");
+Utils.currentDate();
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+import * as Logger from "./logger";
+
+export function currentDate(){
+   Logger.log(`current date is ${new Date()}`);
+}
