52de7bc1c7c06e8e3b0b5448c0d2ef784767458d
FIRST_NAME LAST_NAME <abalaki03@gmail.com>
2022-01-12 20:13:00 +0400

    ---

--- scripts/server.js
+++ scripts/server.js
@@ -1,17 +1,24 @@
-const express = require("express");
-const path = require("path");
-const open = require("open");
+import { express } from "express";
+import { path } from "path";
+import open from "open";
 
 const port = 3000;
 const app = express();
-app.use(express.static('dist'));
 app.get("/", function (req, res) {
-res.sendFile(path.join(__dirname, "../src/index.html"));
+  res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 app.listen(port, function (err) {
-if (err) {
-console.log(err);
-} else {
-open("http://localhost:" + port);
-}
+  if (err) {
+    console.log(err);
+  } else {
+    open("http://localhost:" + port);
+  }
 });
+
+import webpack from "webpack";
+import config from "../webpack.config";
+const compiler = webpack(config);
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+}) );
